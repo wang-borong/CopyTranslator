062d7fed82695ba2fdb921508dfb2c4ca46dcdc3
Jason Wang <wang_borong@163.com>
2026-07-22 14:25:48 +0800

    chore: release version 13.1.0

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -4,7 +4,7 @@ const githubReleases = `${githubRepo}/releases`;
 const constants: { [key: string]: string } = {
   appName: "CopyTranslator",
   nickName: "澄明",
-  version: "13.0.6",
+  version: "13.1.0",
   stage: "stable",
   githubRepo,
   githubReleases,
